fac3900e15d7e483858910858b70e96003a17676
httpcode <blockchaindevweb@gmail.com>
2022-02-06 17:01:17 +0100

    fix: new data structure and refacto

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import styles from '../styles/Home.module.css'
 import Head from 'next/head'
-import { withUrqlClient } from 'next-urql'
-import { useQuery } from 'urql'
+import { createClient } from 'urql'
+import axios from 'axios'
+import CID from 'cids'
 
-const APIURL = process.env.NEXT_PUBLIC_TEMPORARY_QUERY_URL
 
 const tokensQuery = `
   query {
@@ -21,38 +20,53 @@ const tokensQuery = `
   }
 `
 
-export function Home() {
-  const [result] = useQuery({
-    query: tokensQuery
-  })
-
+export default function Home({ data }) {
   const [tokensData, setTokensData] = useState([])
 
   useEffect(async () => {
-    if (!result?.data?.tokens) return
+    if (!data?.tokens) return
 
-    const promises = await Promise.all(result?.data?.tokens?.map(async token => {
-      for await (const tokenId of tokensData) {
-        if (tokenId.id === token.id) return
+    const tokenCID = tokenIpfsLink => {
+      let tokenIpfsCID = tokenIpfsLink?.split('/').pop()
+      if (tokenIpfsLink?.lenght === 46 && tokenIpfsCID?.substring(0, 2) === 'Qm') {
+        tokenIpfsCID = new CID(tokenIpfsLink).toV1().toString('base32')
       }
 
-      return token
-    }))
+      return tokenIpfsCID
+    }
 
-    for (const token of promises) {
+    const ipfsGet = async tokenIpfsLink => {
+      let tokenIpfsCID = tokenCID(tokenIpfsLink)
+
+      let result
+      try {
+        result = await axios.get(`https://${tokenIpfsCID}.ipfs.infura-ipfs.io`)
+      } catch (err) {
+        console.log(err)
+      }
+
+      return result
+    }
+
+    for (const token of data?.tokens) {
       if(!token?.contentURI) return
-      const newContentURI = await fetch(`https://ipfs.io/ipfs/${token.contentURI.split('/').pop()}`)
-      const newMetadataURI = await fetch(`https://ipfs.io/ipfs/${token.metadataURI.split('/').pop()}`)
-      const fetchMetadataURI = await(await fetch(newMetadataURI.url)).json()
 
-      const newToken = { ...token, contentURI: newContentURI.url, metadataURI: fetchMetadataURI }
+      let isTokenExist
+
+      tokensData.map(async item => {
+        if (item?.id === token?.id) isTokenExist = true
+      })
+
+      if (isTokenExist) return
+
+      const newContentURI = `https://${tokenCID(token?.contentURI)}.ipfs.infura-ipfs.io`
+      const newMetadataURI = await ipfsGet(token?.metadataURI)
+
+      const newToken = { ...token, contentURI: newContentURI, metadataURI: newMetadataURI }
 
       setTokensData(state => [...state, newToken])
     }
-
-  }, [result])
-
-console.log(tokensData);
+  }, [data])
 
   return (
     <>
@@ -61,26 +75,29 @@ console.log(tokensData);
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-3 gap-4 px-10 py-10">
-        {tokensData.map(token => {
+        {tokensData?.map(token => {
           return (
-            <div key={token.id} className="shadow-lg bg-transparent rounded-2xl overflow-hidden">
+            <div key={token?.id} className="shadow-lg bg-transparent rounded-2xl overflow-hidden">
               <div className="w-100% h-100%">
-                {token?.metadataURI?.mimeType === 'image' &&
-                  <div style={{height: '320px', overflow: 'hidden'}}>
-                    <img className="object-contain" style={{ minHeight: '320px' }} src={token.contentURI} />
+                {token?.metadataURI?.data?.mimeType === 'image/jpeg'
+                && <div style={{height: '320px', overflow: 'hidden'}}>
+                    <img className="object-contain" style={{ minHeight: '320px' }} src={token?.contentURI} />
                   </div>
                 }
-                {token?.metadataURI?.body?.mimeType === 'audio/wav' &&
-                  <figure>
-                    <figcaption>Listen "{token.metadataURI.body.title}" by {token.metadataURI.body.artist}</figcaption>
+                {token?.metadataURI?.data?.mimeType === 'audio/wav'
+                || token?.metadataURI?.data?.mimeType === 'audio/x-wav'
+                && <figure>
+                    <figcaption>Listen "{token?.metadataURI?.data?.name}" by {token.metadataURI?.data?.artist}</figcaption>
                       <audio controls>
-                        <source src={token.contentURI} type="audio/ogg" />
-                        <source src={token.contentURI} type="audio/mpeg" />
+                        <source src={token?.contentURI} type="audio/ogg" />
+                        <source src={token?.contentURI} type="audio/mpeg" />
                         Your browser does not support the audio element.
                       </audio>
                   </figure>
                 }
-                {token?.metadataURI?.mimeType === 'video/mp4' && (
+                {/* TODO: repair the video player */}
+                {token?.metadataURI?.data?.mimeType === 'video/mp4'
+                && (
                   <div className="relative">
                     <div style={{width: '288px', height: '320px', boxSizing: 'border-box'}} />
                     <div style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0 }}>
@@ -92,15 +109,16 @@ console.log(tokensData);
                         display: 'block',
                         objectFit: 'cover'
                       }}>
-                        <source src={token.contentURI} />
+                        <source src={token?.contentURI} />
                       </video>
                     </div>
                   </div>
                 )}
-                {token?.metadataURI?.mimeType === 'text/plain' && (
+                {token?.metadataURI?.data?.mimeType === 'text/plain'
+                && (
                   <div className="px-2 pt-2 pb-10">
                     <h3 style={{ height: 100 }} className="text-2xl p-4 pt-6 font-semibold">
-                      {token.metadataURI.name}
+                      {token?.metadataURI?.data?.name}
                     </h3>
                   </div>
                 )}
@@ -113,6 +131,18 @@ console.log(tokensData);
   )
 }
 
-export default withUrqlClient((_ssrExchange, ctx) => ({
-  url: APIURL
-}))(Home)
+export const getServerSideProps = async context => {
+  const API_URL = process.env.NEXT_PUBLIC_TEMPORARY_QUERY_URL
+
+  const client = createClient({
+    url: API_URL,
+  })
+
+  const { data } = await client.query(tokensQuery).toPromise()
+
+  return {
+    props: {
+      data: data
+    }
+  }
+}
